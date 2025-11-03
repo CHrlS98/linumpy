4476272b801b00781146700710a6eac28bad8099
CHrlS98 <charles.poirier@usherbrooke.ca>
2025-11-03 10:07:05 +0100

    Crop volume

--- scripts/linum_crop_volume_manual.py
+++ scripts/linum_crop_volume_manual.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Crop volume manually between (xmin, xmax), (ymin, ymin) and (zmin, zmax),
+corresponding to the first, second and third axes in the dataset, respectively.
+"""
 import argparse
 from linumpy.io import save_omezarr, read_omezarr
 
@@ -8,14 +12,22 @@ import dask.array as da
 
 def _build_arg_parser():
     p = argparse.ArgumentParser()
-    p.add_argument('in_volume')
-    p.add_argument('out_volume')
-    p.add_argument('--xmin', default=0, type=int)
-    p.add_argument('--xmax', default=-1, type=int)
-    p.add_argument('--ymin', default=0, type=int)
-    p.add_argument('--ymax', default=-1, type=int)
-    p.add_argument('--zmin', default=0, type=int)
-    p.add_argument('--zmax', default=-1, type=int)
+    p.add_argument('in_volume',
+                   help='Input volume in .ome.zarr format.')
+    p.add_argument('out_volume',
+                   help='Output volume in .ome.zarr format.')
+    p.add_argument('--xmin', default=0, type=int,
+                   help='Minimum index for first axis [%(default)s].')
+    p.add_argument('--xmax', default=-1, type=int,
+                   help='Maximum index for first axis [%(default)s].')
+    p.add_argument('--ymin', default=0, type=int,
+                   help='Minimum index for second axis [%(default)s].')
+    p.add_argument('--ymax', default=-1, type=int,
+                   help='Maximum index for second axis [%(default)s].')
+    p.add_argument('--zmin', default=0, type=int,
+                   help='Minimum index for third axis [%(default)s].')
+    p.add_argument('--zmax', default=-1, type=int,
+                   help='Maximum index for third axis [%(default)s].')
     return p
 
 
